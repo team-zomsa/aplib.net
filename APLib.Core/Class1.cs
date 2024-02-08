9b25785f889cba731090fd7ca0c3a14cd10d4892
Joachim Dekker <jdekker3008@gmail.com>
2024-02-08 10:25:09 +0100

    Add initial library

---
+++ APLib.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace APLib.Core;
+
+/// <summary>
+/// Describes the APLib package.
+/// </summary>
+public class Aplib
+{
+    public string PackageName = "APLib";
+    public int package_version = 1_0_0;
+
+    public void LaadPakket() => Console.WriteLine("APLib is running...");
+}
