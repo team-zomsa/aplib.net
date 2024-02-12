13506c30a5a4c9e14cdaf0cb5d25d303611e374a
Joachim Dekker <51530837+joachimdekker@users.noreply.github.com>
2024-02-12 22:32:58 +0100

    Create Main.cs

---
+++ Aplib.Core/Main.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Aplib.Core
+{
+    public class SonarTest
+    {
+        public void Main()
+        {
+            Console.WriteLine("Hello World!");
+            int i = 0;
+
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.WriteLine("i is 1");
+        }
+    }
+}
