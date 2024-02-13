be1e6bbc8961c965923f8fafa483686512d137e2
Joachim Dekker <jdekker3008@gmail.com>
2024-02-13 16:51:33 +0100

    Just write hello world for the test

--- Aplib.Core/Main.cs
+++ Aplib.Core/Main.cs
@@ -4,18 +4,6 @@ namespace Aplib.Core
 {
     public class SonarTest
     {
-        public void Main()
-        {
-            Console.WriteLine("Hello World!");
-            int i = 0;
-
-            while (i < 10)
-            {
-                Console.WriteLine(i);
-                i++;
-            }
-
-            Console.WriteLine("i is 1");
-        }
+        public void Main() => Console.WriteLine("Hello World!");
     }
 }
