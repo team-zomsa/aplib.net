06ce10474c9dcb437111b8b4e3975244d3eae67f
Joachim Dekker <jdekker3008@gmail.com>
2024-02-27 16:35:40 +0100

    Feat: Add Action

---
+++ Aplib.Core/Action.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Aplib.Core
+{
+    /// <summary>
+    /// Describes an action that can be executed and guarded.
+    /// </summary>
+    /// <typeparam name="TQuery">The query of the action</typeparam>
+    public class Action<TQuery>
+        where TQuery : class
+    {
+        private TQuery? _storedResult;
+
+        /// <summary>
+        /// The effect of the action.
+        /// </summary>
+        public System.Action<TQuery>? Effect { private get; set; }
+
+        /// <summary>
+        /// The query of the action. Can return a value, which is stored and can be used in the effect.
+        /// </summary>
+        /// <remarks>A boolean value can also be used. If an object is used, use null to mark the action as unqualified.</remarks>
+        public Func<TQuery>? Query { private get; set; }
+
+        /// <summary>
+        /// Execute the action against the world.
+        /// </summary>
+        public void Execute() => Effect?.Invoke(_storedResult!);
+
+        /// <summary>
+        /// Guard the action against unwanted execution. The result is stored and can be used in the effect.
+        /// </summary>
+        public void Guard() => _storedResult = Query?.Invoke();
+    }
+}
