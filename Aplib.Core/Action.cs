02d13ded71f9661d7d04cafb36d545a0dfb6f468
Joachim Dekker <jdekker3008@gmail.com>
2024-02-29 15:24:10 +0100

    feat: Add the ability to not have a query/guard

--- Aplib.Core/Action.cs
+++ Aplib.Core/Action.cs
@@ -5,46 +5,49 @@ namespace Aplib.Core
     /// <summary>
     /// Describes an action that can be executed and guarded.
     /// </summary>
-    /// <typeparam name="TQuery">The type of the query of the action</typeparam>
-    public class Action<TQuery>
+    public class Action
     {
-        private TQuery? _storedResult;
-
         /// <summary>
         /// Gets or sets the effect of the action.
         /// </summary>
-        public System.Action<TQuery> Effect { private get; set; }
+        protected System.Action Effect { get; set; }
 
         /// <summary>
-        /// Gets a value indicating whether the action is actionable.
+        /// Gets or sets the guard of the action.
         /// </summary>
-        public bool Actionable => Query!.Invoke() is not (false or null);
+        protected Func<bool> Guard { get; set; } = () => true;
 
         /// <summary>
-        /// Gets or sets the query of the action. Can return a value, which is stored and can be used in the effect.
+        /// Initializes a new empty instance of the <see cref="Action{TQuery}"/> class.
         /// </summary>
-        /// <remarks>A boolean value can also be used. If an object is used, use null to mark the action as unactionable.</remarks>
-        public Func<TQuery> Query { private get; set; }
+        /// <remarks>Only meant for internal use</remarks>
+        protected internal Action()
+        {
+            Effect = () => { };
+            Guard = () => false;
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Action{TQuery}"/> class.
         /// </summary>
         /// <param name="effect">The effect of the action.</param>
-        /// <param name="query">THe query of the action.</param>
-        public Action(System.Action<TQuery> effect, Func<TQuery> query)
-        {
-            Effect = effect;
-            Query = query;
-        }
+        public Action(System.Action effect) => Effect = effect;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Action{TQuery}"/> class.
+        /// </summary>
+        /// <param name="effect">The effect of the action.</param>
+        /// <param name="guard">The guard of the action.</param>
+        public Action(System.Action effect, Func<bool> guard) : this(effect) => Guard = guard;
 
         /// <summary>
         /// Execute the action against the world.
         /// </summary>
-        public void Execute() => Effect.Invoke(_storedResult!);
+        public virtual void Execute() => Effect();
 
         /// <summary>
         /// Guard the action against unwanted execution. The result is stored and can be used in the effect.
         /// </summary>
-        public void Guard() => _storedResult = Query.Invoke();
+        public virtual bool IsActionable() => Guard();
     }
 }
