8ed6556e3bcf63791fe5cb13e4ad6f700e9c7da2
Joachim Dekker <jdekker3008@gmail.com>
2024-03-04 20:58:25 +0100

    feat: add Action (#8)
    * Feat: Add Action
    
    * docs: fix documentation
    
    * Fix: Remove tests and mocks for sonar
    
    * feat: add actionable to action
    
    * Test: Add Tests for Action
    
    * fix: conform to Jens
    
    * refactor: add constructor
    
    * feat: Add the ability to not have a query/guard
    
    * fix: make constructor public
    
    * fix: don't let the build crash because of the tests
    
    * fix: Change visibility to internal
    
    * chore: simplify tests

---
+++ Aplib.Core/Action.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Aplib.Core
+{
+    /// <summary>
+    /// Describes an action that can be executed and guarded.
+    /// </summary>
+    public class Action
+    {
+        /// <summary>
+        /// Gets or sets the effect of the action.
+        /// </summary>
+        protected System.Action Effect { get; set; }
+
+        /// <summary>
+        /// Gets or sets the guard of the action.
+        /// </summary>
+        protected Func<bool> Guard { get; set; } = () => true;
+
+        /// <summary>
+        /// Initializes a new empty instance of the <see cref="Action{TQuery}"/> class.
+        /// </summary>
+        /// <remarks>Only meant for internal use</remarks>
+        protected internal Action()
+        {
+            Effect = () => { };
+            Guard = () => false;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Action{TQuery}"/> class.
+        /// </summary>
+        /// <param name="effect">The effect of the action.</param>
+        public Action(System.Action effect) => Effect = effect;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Action{TQuery}"/> class.
+        /// </summary>
+        /// <param name="effect">The effect of the action.</param>
+        /// <param name="guard">The guard of the action.</param>
+        public Action(System.Action effect, Func<bool> guard) : this(effect) => Guard = guard;
+
+        /// <summary>
+        /// Execute the action against the world.
+        /// </summary>
+        internal virtual void Execute() => Effect();
+
+        /// <summary>
+        /// Guard the action against unwanted execution. The result is stored and can be used in the effect.
+        /// </summary>
+        internal virtual bool IsActionable() => Guard();
+    }
+}
