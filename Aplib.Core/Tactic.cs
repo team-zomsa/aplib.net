e501959ce39b2c5bccaf3c61c6ad939ddf87afdf
Thijs Boerefijn <tboefijnb2@gmail.com>
2024-03-05 13:28:54 +0100

    Add guards to the Tactic

--- Aplib.Core/Tactic.cs
+++ Aplib.Core/Tactic.cs
@@ -15,10 +15,33 @@ namespace Aplib.Core
         public TacticType TacticType { get; set; }
         private LinkedList<Tactic> _subTactics { get; set; }
 
+        /// <summary>
+        /// Gets or sets the guard of the tactic.
+        /// </summary>
+        protected Func<bool> Guard { get; set; } = () => true;
         public Tactic(TacticType tacticType, List<Tactic> subTactics)
         {
             TacticType = tacticType;
             _subTactics = new();
+
+            foreach (Tactic tactic in subTactics)
+            {
+                tactic.Parent = this;
+                _ = _subTactics.AddLast(tactic);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Tactic"/> class with the specified tactic type, sub-tactics, and guard.
+        /// </summary>
+        /// <param name="tacticType">The type of the tactic.</param>
+        /// <param name="subTactics">The sub-tactics of the tactic.</param>
+        /// <param name="guard">The guard of the tactic.</param>
+        public Tactic(TacticType tacticType, List<Tactic> subTactics, Func<bool> guard)
+        {
+            TacticType = tacticType;
+            _subTactics = new();
+            Guard = guard;
 
             foreach (Tactic tactic in subTactics)
             {
@@ -71,6 +94,12 @@ namespace Aplib.Core
 
             return primitiveTactics;
         }
+
+        /// <summary>
+        /// Determines whether the tactic is actionable.
+        /// </summary>
+        /// <returns>True if the tactic is actionable, false otherwise.</returns>
+        public bool IsActionable() => Guard();
     }
 
     public class PrimitiveTactic : Tactic
