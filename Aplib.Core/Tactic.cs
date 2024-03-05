c5abb3680cd9c84d55cd57fa247d4314462f5cfa
Thijs Boerefijn <tboefijnb2@gmail.com>
2024-03-05 16:51:34 +0100

    Make Action public

--- Aplib.Core/Tactic.cs
+++ Aplib.Core/Tactic.cs
@@ -121,21 +121,21 @@ namespace Aplib.Core
         /// <summary>
         /// Gets or sets the action of the primitive tactic.
         /// </summary>
-        private readonly Action _action;
+        public readonly Action Action;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PrimitiveTactic"/> class with the specified action.
         /// </summary>
         /// <param name="action">The action of the primitive tactic.</param>
-        public PrimitiveTactic(Action action) : base(TacticType.Primitive, new()) => _action = action;
+        public PrimitiveTactic(Action action) : base(TacticType.Primitive, new()) => Action = action;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PrimitiveTactic"/> class with the specified action and guard.
         /// </summary>
         /// <param name="action">The action of the primitive tactic.</param>
         /// <param name="guard">The guard of the tactic.</param>
-        public PrimitiveTactic(Action action, Func<bool> guard) : base(TacticType.Primitive, new(), guard) => _action = action;
+        public PrimitiveTactic(Action action, Func<bool> guard) : base(TacticType.Primitive, new(), guard) => Action = action;
 
-        public override bool IsActionable() => Guard() && _action.IsActionable();
+        public override bool IsActionable() => Guard() && Action.IsActionable();
     }
 }
