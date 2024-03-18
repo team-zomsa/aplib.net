87b1a949c36a6b92c817f15c8ed8687850681d28
Jens Steenmetz <jj.steenmetz@gmail.com>
2024-03-18 22:59:33 +0100

    test: add tests for Beliefset

---
+++ Aplib.Tests/Core/Belief/BeliefsetTests.cs
@@ -0,0 +1,44 @@
+﻿using Aplib.Core.Belief;
+
+namespace Aplib.Tests.Core.Belief;
+public class BeliefsetTests
+{
+    /// <summary>
+    /// Given a Beliefset instance with multiple beliefs,
+    /// When UpdateBeliefs is called,
+    /// Then all beliefs are updated.
+    /// </summary>
+    [Fact]
+    public void UpdateBeliefs_UpdatesAllBeliefs_WhenCalled()
+    {
+        // Arrange
+        var beliefset = new TestBeliefset();
+
+        // Act
+        beliefset.UpdateBeliefs();
+
+        // Assert
+        Assert.True(beliefset.Belief1.Updated);
+        Assert.True(beliefset.Belief2.Updated);
+    }
+
+    private class TestBeliefset : Beliefset
+    {
+        public SimpleBelief Belief1 = new();
+        public SimpleBelief Belief2 = new();
+
+    }
+
+    /// <summary>
+    /// A simple belief that can be used to test whether <see cref="UpdateBelief"/> has been called.
+    /// </summary>
+    private class SimpleBelief : IBelief
+    {
+        public bool Updated { get; private set; } = false;
+
+        public void UpdateBelief()
+        {
+            Updated = true;
+        }
+    }
+}
