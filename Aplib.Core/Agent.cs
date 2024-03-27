2d4cedcc6fe3f57579854d878b7bb77fe42b91c1
Joachim Dekker <jdekker3008@gmail.com>
2024-03-27 10:18:35 +0100

    refactor: change agent to comply with new code

--- Aplib.Core/Agent.cs
+++ Aplib.Core/Agent.cs
@@ -1,41 +1,47 @@
+using Aplib.Core.Belief;
 using Aplib.Core.Desire;
-using Aplib.Core.Stubs;
+using Aplib.Core.Desire.Goals;
+using Aplib.Core.Intent.Actions;
+using Aplib.Core.Intent.Tactics;
 
 namespace Aplib.Core
 {
     /// <summary>
     /// Represents an agent that performs actions based on goals and beliefs.
     /// </summary>
-    public class Agent : IAgent
+    public class BdiAgent : IAgent
     {
         /// <summary>
         /// Gets the beliefset of the agent.
         /// </summary>
-        public Beliefset State { get; }
+        private BeliefSet _state { get; }
 
         /// <summary>
-        /// Gets the goal structure of the agent.
+        /// Gets the desire of the agent.
         /// </summary>
-        public GoalStructure Goals { get; }
+        /// <remarks>
+        /// The desire contains all goal structures and the current goal.
+        /// </remarks>
+        private DesireSet _desire { get; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Agent"/> class.
         /// </summary>
         /// <param name="state">The beliefset of the agent.</param>
-        /// <param name="goals">The goal structure of the agent.</param>
-        public Agent(Beliefset state, GoalStructure goals)
+        /// <param name="desire"></param>
+        public Agent(BeliefSet state, DesireSet desire)
         {
-            State = state;
-            Goals = goals;
+            _state = state;
+            _desire = desire;
         }
 
         /// <inheritdoc/>
         public void Update()
         {
             // Update the beliefset
-            State.UpdateBeliefs();
+            _state.UpdateBeliefs();
 
-            Goal goal = Goals.GetCurrentGoal();
+            Goal goal = _desire.GetCurrentGoal();
             Tactic tactic = goal.Tactic;
             Action action = tactic.GetAction();
 
