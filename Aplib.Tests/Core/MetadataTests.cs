a7af1b0d7d2fa2485a3a1e151bec4612f7868450
Luka <luukberkers@protonmail.com>
2024-03-27 14:05:53 +0100

    test: metadata identifiers

--- Aplib.Tests/Core/MetadataTests.cs
+++ Aplib.Tests/Core/MetadataTests.cs
@@ -17,6 +17,7 @@ public class MetadataTests
 
         // Assert
         data.Should().NotBeNull();
+        data.Id.Should().NotBeEmpty();
         data.Name.Should().Be(name);
         data.Description.Should().Be(description);
     }
@@ -32,7 +33,67 @@ public class MetadataTests
         
         // Assert
         data.Should().NotBeNull();
+        data.Id.Should().NotBeEmpty();
         data.Name.Should().Be(name);
         data.Description.Should().BeNull();
+    }
+
+    [Fact]
+    public void Metadata_WithoutName_HasNoName()
+    {
+        // Arrange
+        const string description = "An unnamed component";
+
+        // Act
+        Metadata data = new(null, description);
+
+        // Assert
+        data.Should().NotBeNull();
+        data.Id.Should().NotBeEmpty();
+        data.Name.Should().BeNull();
+        data.Description.Should().Be(description);
+    }
+
+    [Fact]
+    public void Metadata_MultipleInstances_HaveUniqueIds()
+    {
+        // Act
+        Metadata data1 = new();
+        Metadata data2 = new();
+
+        // Assert
+        data1.Should().NotBeNull();
+        data1.Id.Should().NotBeEmpty();
+        data1.Name.Should().BeNull();
+        data1.Description.Should().BeNull();
+
+        data2.Should().NotBeNull();
+        data2.Id.Should().NotBeEmpty();
+        data2.Name.Should().BeNull();
+        data2.Description.Should().BeNull();
+
+        data1.Id.Should().NotBe(data2.Id);
+    }
+
+    [Fact]
+    public void Metadata_SameInstance_HasSameId()
+    {
+        // Act
+        Metadata data = new();
+        // ReSharper disable once InlineTemporaryVariable
+        Metadata copy = data;
+
+        // Assert
+        data.Should().NotBeNull();
+        data.Id.Should().NotBeEmpty();
+        data.Name.Should().BeNull();
+        data.Description.Should().BeNull();
+
+        copy.Should().NotBeNull();
+        copy.Id.Should().NotBeEmpty();
+        copy.Name.Should().BeNull();
+        copy.Description.Should().BeNull();
+
+        data.Id.Should().Be(copy.Id);
     }
 }
