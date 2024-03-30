32b1c94bbc1b60c28dbdc9c9e2dc08a526750f90
Joachim Dekker <jdekker3008@gmail.com>
2024-03-30 23:05:40 +0100

    feat: simplify goal structure

--- Aplib.Core/Desire/IGoalStructure.cs
+++ Aplib.Core/Desire/IGoalStructure.cs
@@ -1,9 +1,15 @@
 ﻿using Aplib.Core.Belief;
 using Aplib.Core.Desire.Goals;
-using System;
 
 namespace Aplib.Core.Desire
 {
+    /// <summary>
+    /// Represents a goal structure.
+    /// </summary>
+    /// <remarks>
+    /// A goal structure is structure of predicates that must be fulfilled in order to complete a test.
+    /// </remarks>
+    /// <typeparam name="TBeliefSet"></typeparam>
     public interface IGoalStructure<TBeliefSet> : IInterruptable<TBeliefSet>
         where TBeliefSet : IBeliefSet
     {
@@ -16,29 +22,17 @@ namespace Aplib.Core.Desire
         /// </remarks>
         GoalStructureState State { get; }
 
-        /// <inheritdoc />
-        event EventHandler<InterruptableEventArgs<TBeliefSet>> OnInterrupt;
-
-        /// <inheritdoc />
-        event EventHandler<InterruptableEventArgs<TBeliefSet>> OnReinstate;
-
-        /// <inheritdoc />
-        void Interrupt(TBeliefSet beliefSet);
-
-        /// <inheritdoc />
-        void Reinstate(TBeliefSet beliefSet);
-
-        /// <summary>
-        /// Updates the state of the goal structure.
-        /// </summary>
-        /// <param name="beliefSet">The belief set of the agent.</param>
-        void UpdateState(TBeliefSet beliefSet);
-
         /// <summary>
         /// Gets the current goal using the given <see cref="IBeliefSet" />.
         /// </summary>
         /// <param name="beliefSet">The belief set of the agent.</param>
         /// <returns>The current goal to be fulfilled.</returns>
         IGoal? GetCurrentGoal(TBeliefSet beliefSet);
+
+        /// <summary>
+        /// Updates the state of the goal structure.
+        /// </summary>
+        /// <param name="beliefSet">The belief set of the agent.</param>
+        void UpdateState(TBeliefSet beliefSet);
     }
 }
