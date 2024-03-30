4e6d858d7b5e4f9fae818f07078827f050d38e29
Joachim Dekker <jdekker3008@gmail.com>
2024-03-30 23:10:32 +0100

    feat: make Goal and Goalstructure inherit from interfaces.

--- Aplib.Core/Desire/GoalStructure.cs
+++ Aplib.Core/Desire/GoalStructure.cs
@@ -1,5 +1,6 @@
 using Aplib.Core.Belief;
 using Aplib.Core.Desire.Goals;
+using System;
 using System.Collections.Generic;
 
 namespace Aplib.Core.Desire
@@ -7,70 +8,67 @@ namespace Aplib.Core.Desire
     /// <summary>
     /// Describes a structure of goals that need to be fulfilled.
     /// </summary>
-    /// <remarks>
-    ///
-    /// </remarks>
-    public abstract class GoalStructure<TBeliefSet>
-        where TBeliefSet : IBeliefSet
+    public abstract class GoalStructure<TBeliefSet> : IGoalStructure<TBeliefSet> where TBeliefSet : IBeliefSet
     {
-        /// <summary>
-        /// The children of the goal structure.
-        /// </summary>
-        protected IList<GoalStructure<TBeliefSet>> _children;
+        /// <inheritdoc />
+        public event EventHandler<InterruptableEventArgs<TBeliefSet>>? OnInterrupt;
 
-        /// <summary>
-        /// The goalstructure that is currently being fulfilled.
-        /// </summary>
-        protected GoalStructure<TBeliefSet>? _currentGoalStructure;
+        /// <inheritdoc />
+        public event EventHandler<InterruptableEventArgs<TBeliefSet>>? OnReinstate;
 
         /// <summary>
         /// Gets or sets the state of the goal structure.
         /// </summary>
         /// <remarks>
-        /// By default, the state is set to <see cref="GoalStructureState.Unfinished"/>.
-        /// However, this can be changed by the goal structure itself.
+        /// By default, the state is set to <see cref="GoalStructureState.Unfinished" />.
+        /// However, this can be changed by the goal structure.
         /// </remarks>
-        public GoalStructureState State { get; protected set; } = GoalStructureState.Unfinished; // TODO
+        public GoalStructureState State { get; protected set; } = GoalStructureState.Unfinished;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="GoalStructure"/> class.
+        /// The children of the goal structure.
+        /// </summary>
+        protected readonly IEnumerable<IGoalStructure<TBeliefSet>> _children;
+
+        /// <summary>
+        /// The goalstructure that is currently being fulfilled.
+        /// </summary>
+        protected IGoalStructure<TBeliefSet>? _currentGoalStructure;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GoalStructure{TBeliefSet}" /> class.
         /// </summary>
         /// <param name="children">The children of the goal structure.</param>
-        protected GoalStructure(IList<GoalStructure<TBeliefSet>> children)
-        {
-            _children = children;
-        }
+        protected GoalStructure(IEnumerable<IGoalStructure<TBeliefSet>> children) => _children = children;
 
         /// <summary>
-        /// Interrupts the current goal structure.
-        /// </summary>
-        public void Interrupt()
-        {
-            foreach (GoalStructure<TBeliefSet> child in _children)
-                child.Interrupt();
-
-            ProcessInterrupt();
-        }
-
-        /// <summary>
-        /// Gets the current goal using the given <see cref="IBeliefSet"/>.
+        /// Gets the current goal using the given <see cref="IBeliefSet" />.
         /// </summary>
         /// <param name="beliefSet">The belief set of the agent.</param>
         /// <returns>The current goal to be fulfilled.</returns>
-        public abstract Goal? GetCurrentGoal(TBeliefSet beliefSet);
+        public abstract IGoal? GetCurrentGoal(TBeliefSet beliefSet);
+
+        /// <inheritdoc />
+        public void Interrupt(TBeliefSet beliefSet)
+        {
+            OnInterrupt?.Invoke(this, new InterruptableEventArgs<TBeliefSet>(beliefSet));
+
+            foreach (IGoalStructure<TBeliefSet> child in _children)
+                child.Interrupt(beliefSet);
+        }
+
+        /// <inheritdoc />
+        public void Reinstate(TBeliefSet beliefSet)
+        {
+            OnReinstate?.Invoke(this, new InterruptableEventArgs<TBeliefSet>(beliefSet));
+            foreach (IGoalStructure<TBeliefSet> child in _children)
+                child.Reinstate(beliefSet);
+        }
 
         /// <summary>
-        /// Processes the interrupt thrown by the <see cref="DesireSet"/>.
+        /// Updates the state of the goal structure.
         /// </summary>
-        /// <remarks>
-        ///<para>
-        /// A goal structure may want to do something when it is interrupted.
-        /// This method is always called when another goal structure interrupts this one.
-        ///</para>
-        /// <para>
-        /// The default implementation does nothing.
-        /// </para>
-        /// </remarks>
-        protected virtual void ProcessInterrupt() { }
+        /// <param name="beliefSet">The belief set of the agent.</param>
+        public abstract void UpdateState(TBeliefSet beliefSet);
     }
 }
--- Aplib.Core/Desire/Goals/Goal.cs
+++ Aplib.Core/Desire/Goals/Goal.cs
@@ -6,31 +6,18 @@ namespace Aplib.Core.Desire.Goals
 {
     /// <summary>
     /// A goal effectively combines a heuristic function with a tactic, and aims to meet the heuristic function by
-    /// applying the tactic. Goals are combined in a <see cref="GoalStructure"/>, and are used to prepare tests or do
+    /// applying the tactic. Goals are combined in a <see cref="GoalStructure" />, and are used to prepare tests or do
     /// the testing.
     /// </summary>
-    /// <seealso cref="GoalStructure"/>
-    public class Goal
+    /// <seealso cref="GoalStructure" />
+    public class Goal : IGoal
     {
         /// <summary>
-        /// The abstract definition of what is means to test the Goal's heuristic function. Returns <see cref="Heuristics"/>, as
+        /// The abstract definition of what is means to test the Goal's heuristic function. Returns <see cref="Heuristics" />, as
         /// they represent how close we are to matching the heuristic function, and if the goal is completed.
         /// </summary>
-        /// <seealso cref="Goal.GetState"/>
+        /// <seealso cref="Goal.GetState" />
         public delegate Heuristics HeuristicFunction(IBeliefSet beliefSet);
-
-
-        /// <summary>
-        /// Gets the <see cref="Heuristics"/> of the current state of the game.
-        /// </summary>
-        /// <remarks>If no heuristics have been calculated yet, they will be calculated first.</remarks>
-        public virtual Heuristics CurrentHeuristics(IBeliefSet beliefSet)
-            => _currentHeuristics ??= _heuristicFunction.Invoke(beliefSet);
-
-        /// <summary>
-        /// The name used to display the current goal during debugging, logging, or general overviews.
-        /// </summary>
-        public string Name { get; }
 
         /// <summary>
         /// The description used to describe the current goal during debugging, logging, or general overviews.
@@ -38,42 +25,49 @@ namespace Aplib.Core.Desire.Goals
         public string Description { get; }
 
         /// <summary>
-        /// The goal is considered to be completed, when the distance of the <see cref="CurrentHeuristics"/> is below
+        /// The name used to display the current goal during debugging, logging, or general overviews.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// The <see cref="Intent.Tactics.Tactic" /> used to achieve this <see cref="Goal" />, which is executed during every
+        /// iteration
+        /// of the BDI cycle.
+        /// </summary>
+        public Tactic Tactic { get; }
+
+        /// <summary>
+        /// The goal is considered to be completed, when the distance of the <see cref="CurrentHeuristics" /> is below
         /// this value.
         /// </summary>
         protected double _epsilon { get; }
 
 
         /// <summary>
-        /// The concrete implementation of this Goal's <see cref="HeuristicFunction"/>. Used to test whether this goal is
+        /// The concrete implementation of this Goal's <see cref="HeuristicFunction" />. Used to test whether this goal is
         /// completed.
         /// </summary>
-        /// <seealso cref="GetState"/>
+        /// <seealso cref="GetState" />
         protected HeuristicFunction _heuristicFunction;
 
         /// <summary>
-        /// The <see cref="Intent.Tactics.Tactic"/> used to achieve this <see cref="Goal"/>, which is executed during every iteration
-        /// of the BDI cycle.
-        /// </summary>
-        public Tactic Tactic { get; private set; }
-
-        /// <summary>
-        /// The backing field of <see cref="Heuristics"/>.
+        /// The backing field of <see cref="Heuristics" />.
         /// </summary>
         private Heuristics? _currentHeuristics;
 
         /// <summary>
-        /// Creates a new goal which works with <see cref="Heuristics"/>.
+        /// Creates a new goal which works with <see cref="Heuristics" />.
         /// </summary>
         /// <param name="tactic">The tactic used to approach this goal.</param>
         /// <param name="heuristicFunction">The heuristic function which defines whether a goal is reached</param>
         /// <param name="name">The name of this goal, used to quickly display this goal in several contexts.</param>
         /// <param name="description">The description of this goal, used to explain this goal in several contexts.</param>
         /// <param name="epsilon">
-        /// The goal is considered to be completed, when the distance of the <see cref="CurrentHeuristics"/> is below
+        /// The goal is considered to be completed, when the distance of the <see cref="CurrentHeuristics" /> is below
         /// this value.
         /// </param>
-        public Goal(Tactic tactic, HeuristicFunction heuristicFunction, string name, string description, double epsilon = 0.005d)
+        public Goal(Tactic tactic, HeuristicFunction heuristicFunction, string name, string description,
+            double epsilon = 0.005d)
         {
             Tactic = tactic;
             _heuristicFunction = heuristicFunction;
@@ -83,14 +77,14 @@ namespace Aplib.Core.Desire.Goals
         }
 
         /// <summary>
-        /// Creates a new goal which works with boolean-based <see cref="Heuristics"/>.
+        /// Creates a new goal which works with boolean-based <see cref="Heuristics" />.
         /// </summary>
         /// <param name="tactic">The tactic used to approach this goal.</param>
         /// <param name="predicate">The heuristic function (or specifically predicate) which defines whether a goal is reached.</param>
         /// <param name="name">The name of this goal, used to quickly display this goal in several contexts.</param>
         /// <param name="description">The description of this goal, used to explain this goal in several contexts.</param>
         /// <param name="epsilon">
-        /// The goal is considered to be completed, when the distance of the <see cref="CurrentHeuristics"/> is below
+        /// The goal is considered to be completed, when the distance of the <see cref="CurrentHeuristics" /> is below
         /// this value.
         /// </param>
         public Goal(Tactic tactic, Func<bool> predicate, string name, string description, double epsilon = 0.005d)
@@ -102,17 +96,23 @@ namespace Aplib.Core.Desire.Goals
             _epsilon = epsilon;
         }
 
+
         /// <summary>
-        /// Tests whether the goal has been achieved, bases on the <see cref="_heuristicFunction"/> and the
-        /// <see cref="CurrentHeuristics"/>. When the distance of the heuristics is smaller than <see cref="_epsilon"/>,
+        /// Gets the <see cref="Heuristics" /> of the current state of the game.
+        /// </summary>
+        /// <remarks>If no heuristics have been calculated yet, they will be calculated first.</remarks>
+        public virtual Heuristics CurrentHeuristics(IBeliefSet beliefSet)
+            => _currentHeuristics ??= _heuristicFunction.Invoke(beliefSet);
+
+        /// <summary>
+        /// Tests whether the goal has been achieved, bases on the <see cref="_heuristicFunction" /> and the
+        /// <see cref="CurrentHeuristics" />. When the distance of the heuristics is smaller than <see cref="_epsilon" />,
         /// the goal is considered to be completed.
         /// </summary>
         /// <returns>An enum representing whether the goal is complete and if so, with what result.</returns>
-        /// <seealso cref="_epsilon"/>
-        public GoalState GetState(IBeliefSet beliefSet) => CurrentHeuristics(beliefSet).Distance < _epsilon
+        /// <seealso cref="_epsilon" />
+        public virtual GoalState GetState(IBeliefSet beliefSet) => CurrentHeuristics(beliefSet).Distance < _epsilon
             ? GoalState.Success
-            : GoalState.Unfinished; // TODO what about failure?
+            : GoalState.Unfinished;
     }
-
-    public enum GoalState { Unfinished, Success, Failure }
 }
