2b3f445306dcb7605f5c8a8ec5d3b7c5c6cd4d91
Jens Steenmetz <jj.steenmetz@gmail.com>
2024-04-01 15:03:32 +0200

    feat: add UpdateStatus to DesireSet

--- Aplib.Core/Desire/DesireSet.cs
+++ Aplib.Core/Desire/DesireSet.cs
@@ -7,23 +7,30 @@ namespace Aplib.Core.Desire
     public class DesireSet<TBeliefSet> : IDesireSet<TBeliefSet>
         where TBeliefSet : IBeliefSet
     {
+        /// <summary>
+        /// Stores the main goal structure of the agent.
+        /// </summary>
+        private IGoalStructure<TBeliefSet> _mainGoal { get; }
+
         /// <inheritdoc />
-        public GoalStructure<TBeliefSet> MainGoal { get; }
-        
-        /// <inheritdoc />
-        public CompletionStatus Status => MainGoal.Status;
-        
+        public CompletionStatus Status => _mainGoal.Status;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="DesireSet{TBeliefSet}" /> class.
         /// </summary>
         /// <param name="mainGoal">The main goal structure that the agent needs to complete.</param>
-        public DesireSet(GoalStructure<TBeliefSet> mainGoal) => MainGoal = mainGoal;
+        public DesireSet(IGoalStructure<TBeliefSet> mainGoal) => _mainGoal = mainGoal;
 
         /// <inheritdoc />
         public IGoal GetCurrentGoal(TBeliefSet beliefSet)
         {
-            MainGoal.UpdateStatus(beliefSet);
-            return MainGoal.GetCurrentGoal(beliefSet);
+            return _mainGoal.GetCurrentGoal(beliefSet);
+        }
+
+        /// <inheritdoc />
+        public void UpdateStatus(TBeliefSet beliefSet)
+        {
+            _mainGoal.UpdateStatus(beliefSet);
         }
     }
 }
--- Aplib.Core/Desire/IDesireSet.cs
+++ Aplib.Core/Desire/IDesireSet.cs
@@ -8,19 +8,20 @@ namespace Aplib.Core.Desire
     /// This is the main structure that the agent will use to determine what it should do next.
     /// </summary>
     /// <typeparam name="TBeliefSet"></typeparam>
-    public interface IDesireSet<TBeliefSet> :  ICompletable
+    public interface IDesireSet<TBeliefSet> : ICompletable
         where TBeliefSet : IBeliefSet
     {
-        /// <summary>
-        /// Stores the main goal structure of the agent.
-        /// </summary>
-        GoalStructure<TBeliefSet> MainGoal { get; }
-        
         /// <summary>
         /// Gets the current goal using the given <see cref="IBeliefSet" />.
         /// </summary>
         /// <param name="beliefSet">The belief set of the agent.</param>
         /// <returns>The current goal to be fulfilled.</returns>
         IGoal? GetCurrentGoal(TBeliefSet beliefSet);
+
+        /// <summary>
+        /// Updates the status of this <see cref="IDesireSet"/>.
+        /// </summary>
+        /// <param name="beliefSet">The belief set of the agent.</param>
+        void UpdateStatus(TBeliefSet beliefSet);
     }
 }
