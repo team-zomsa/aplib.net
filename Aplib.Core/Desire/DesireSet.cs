761d9270ed3b79c2c3dbaeeef5b814bfd5abfe8a
Jens Steenmetz <jj.steenmetz@gmail.com>
2024-04-01 14:02:13 +0200

    feat: add DesireSet

---
+++ Aplib.Core/Desire/DesireSet.cs
@@ -0,0 +1,23 @@
+﻿using Aplib.Core.Belief;
+using Aplib.Core.Desire.Goals;
+using System.Data;
+
+namespace Aplib.Core.Desire
+{
+    public class DesireSet<TBeliefSet> : IDesireSet<TBeliefSet>
+        where TBeliefSet : IBeliefSet
+    {
+        public GoalStructure<TBeliefSet> MainGoal { get; }
+        
+        /// <inheritdoc />
+        public CompletionStatus Status => MainGoal.Status;
+        
+        public DesireSet(GoalStructure<TBeliefSet> mainGoal) => MainGoal = mainGoal;
+
+        public IGoal GetCurrentGoal(TBeliefSet beliefSet)
+        {
+            MainGoal.UpdateStatus(beliefSet);
+            return MainGoal.GetCurrentGoal(beliefSet);
+        }
+    }
+}
---
+++ Aplib.Core/Desire/IDesireSet.cs
@@ -0,0 +1,19 @@
+﻿using Aplib.Core.Belief;
+using Aplib.Core.Desire.Goals;
+using System;
+
+namespace Aplib.Core.Desire
+{
+    public interface IDesireSet<TBeliefSet> :  ICompletable
+        where TBeliefSet : IBeliefSet
+    {
+        GoalStructure<TBeliefSet> MainGoal { get; }
+        
+        /// <summary>
+        /// Gets the current goal using the given <see cref="IBeliefSet" />.
+        /// </summary>
+        /// <param name="beliefSet">The belief set of the agent.</param>
+        /// <returns>The current goal to be fulfilled.</returns>
+        IGoal? GetCurrentGoal(TBeliefSet beliefSet);
+    }
+}
