29c0a947a89d733d84457d78c8f4ba85b141d846
Luka <51298607+LuukBerkers@users.noreply.github.com>
2024-04-01 22:43:37 +0200

    feat: add Agent (#14)
    * Fix: Remove tests and mocks for sonar
    
    * chore: make git preserve correct line endings
    
    * chore: ignore .idea directory
    
    * chore: add ‘Aplib’ to dictionary
    
    * feat: agent basic interface
    
    * Revert "chore: ignore .idea directory"
    
    This reverts commit 5a78a63733ae2d03b653e64b44e7e35cc88abb65.
    
    * Revert "chore: make git preserve correct line endings"
    
    This reverts commit c508a74126a03ec5a28f00bceeaba7bb167e4fd3.
    
    * feat: iterate goal on agent update
    
    * fix: remove budget from agent
    
    * feat: update agent to new standard document
    
    * fix: make the build
    
    * chore: remove stubs
    
    * feat: add (stubby) desireset
    
    * refactor: change agent to comply with new code
    
    * fix: bug due to name change
    
    * rafactor: rename Agent to BdiAgent
    
    * fix: change casing in Action.cs
    
    * feat: complete agent
    
    * test: test the agent
    
    * feat: conform to Sonar
    
    * fix: delete .dotsettings
    
    * chore: reflect requested changes
    
    * fix: make action nullable
    
    * tests: fix broken tests
    
    * refactor: change names
    
    * docs: provide more clear documentation.
    
    * chore: fix spacing
    
    Co-authored-by: Jens Steenmetz <35835627+JensSteenmetz@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Joachim Dekker <jdekker3008@gmail.com>
    Co-authored-by: Jens Steenmetz <35835627+JensSteenmetz@users.noreply.github.com>

--- Aplib.Core/Intent/Actions/Action.cs
+++ Aplib.Core/Intent/Actions/Action.cs
@@ -10,44 +10,44 @@ namespace Aplib.Core.Intent.Actions
         /// <summary>
         /// Gets or sets the effect of the action.
         /// </summary>
-        protected System.Action Effect { get; set; }
+        protected System.Action _effect { get; set; }
 
         /// <summary>
         /// Gets or sets the guard of the action.
         /// </summary>
-        protected Func<bool> Guard { get; set; } = () => true;
+        protected Func<bool> _guard { get; set; } = () => true;
 
         /// <summary>
-        /// Initializes a new empty instance of the <see cref="Action{TQuery}"/> class.
+        /// Initializes a new instance of the <see cref="Action{TQuery}" /> class.
+        /// </summary>
+        /// <param name="effect">The effect of the action.</param>
+        public Action(System.Action effect) => _effect = effect;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Action{TQuery}" /> class.
+        /// </summary>
+        /// <param name="effect">The effect of the action.</param>
+        /// <param name="guard">The guard of the action.</param>
+        public Action(System.Action effect, Func<bool> guard) : this(effect) => _guard = guard;
+
+        /// <summary>
+        /// Initializes a new empty instance of the <see cref="Action{TQuery}" /> class.
         /// </summary>
         /// <remarks>Only meant for internal use</remarks>
         protected internal Action()
         {
-            Effect = () => { };
-            Guard = () => false;
+            _effect = () => { };
+            _guard = () => false;
         }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Action{TQuery}"/> class.
-        /// </summary>
-        /// <param name="effect">The effect of the action.</param>
-        public Action(System.Action effect) => Effect = effect;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Action{TQuery}"/> class.
-        /// </summary>
-        /// <param name="effect">The effect of the action.</param>
-        /// <param name="guard">The guard of the action.</param>
-        public Action(System.Action effect, Func<bool> guard) : this(effect) => Guard = guard;
 
         /// <summary>
         /// Execute the action against the world.
         /// </summary>
-        internal virtual void Execute() => Effect();
+        internal virtual void Execute() => _effect();
 
         /// <summary>
         /// Guard the action against unwanted execution. The result is stored and can be used in the effect.
         /// </summary>
-        internal virtual bool IsActionable() => Guard();
+        internal virtual bool IsActionable() => _guard();
     }
 }
