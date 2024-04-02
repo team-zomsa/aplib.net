7ea5f7f741e1906c351a584166d9d03a4e082d65
Jens Steenmetz <jj.steenmetz@gmail.com>
2024-04-02 21:25:26 +0200

    feat: add IAction and ITactic

---
+++ Aplib.Core/Intent/Tactics/ITactic.cs
@@ -0,0 +1,24 @@
+﻿using Aplib.Core.Belief;
+using Aplib.Core.Intent.Actions;
+
+namespace Aplib.Core.Intent.Tactics
+{
+    /// <summary>
+    /// Represents a tactic that an agent can use to achieve its goals.
+    /// A tactic is a strategy for achieving a particular goal.
+    /// </summary>
+    /// <typeparam name="TBeliefSet">The type of the belief set that the tactic uses.</typeparam>
+    public interface ITactic<in TBeliefSet> where TBeliefSet : IBeliefSet
+    {
+        /// <summary>
+        /// Gets the first enabled action of the tactic.
+        /// An action is a specific operation that an agent can perform.
+        /// The action returned by this method is the first action that the tactic can perform,
+        /// given the current state of the world as represented by the belief set.
+        /// </summary>
+        /// <returns>
+        /// A concrete <see cref="IAction{TBeliefSet}"/> that the tactic can perform, or null if no actions are enabled.
+        /// </returns>
+        public IAction<TBeliefSet>? GetAction(TBeliefSet beliefSet);
+    }
+}
