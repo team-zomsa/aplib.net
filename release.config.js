1e9f8db50bde2d606e8001182206fad0ba741fa2
Quake <QuakeEye@users.noreply.GitHub.com>
2024-04-29 15:55:38 +0200

    feat: introduce dev branch and pipelines

--- release.config.js
+++ release.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-    "plugins": [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/github",
-      [
-        "@semantic-release/exec"
-      ]
-    ],
-    "branches": [
-      "main"
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    [
+      "@semantic-release/exec"
     ]
-  }
+  ],
+  "branches": [
+    { name: "main" },
+    { name: "dev", channel: "dev", prerelease: true }
+  ]
+}
