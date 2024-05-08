e0fc36c07e1b0ced38e62a2c01da46666aaf5c34
Joachim Dekker <jdekker3008@gmail.com>
2024-05-08 13:26:58 +0200

    feat: Add IPathfinder

---
+++ Aplib.Extensions/IPathfinder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aplib.Extensions
+{
+    public interface IPathfinder<T>
+    {
+        public ReadOnlySpan<T> FindPath(T begin, T end);
+
+        public T GetNextStep(T current, T end);
+
+        public bool TryGetNextStep(T current, T end, out T nextStep);
+    }
+}
