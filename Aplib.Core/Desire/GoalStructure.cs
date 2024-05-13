a141572f6c7737bc36032dae754a81fae095f60e
Luka <luukberkers@protonmail.com>
2024-05-13 16:25:38 +0200

    feat(GoalStructure): add `Metadata` property and parameter to constructor

--- Aplib.Core/Desire/GoalStructure.cs
+++ Aplib.Core/Desire/GoalStructure.cs
@@ -15,19 +15,34 @@ namespace Aplib.Core.Desire
         /// </summary>
         protected readonly IEnumerable<IGoalStructure<TBeliefSet>> _children;
 
+        /// <inheritdoc />
+        public CompletionStatus Status { get; protected set; }
+
+        /// <summary>
+        /// Gets the metadata of the GoalStructure.
+        /// </summary>
+        /// <remark>
+        /// This metadata may be useful for debugging or logging.
+        /// </remark>
+        public Metadata Metadata { get; }
+
         /// <summary>
         /// The goal structure that is currently being fulfilled.
         /// </summary>
         protected IGoalStructure<TBeliefSet>? _currentGoalStructure;
 
-        /// <inheritdoc />
-        public CompletionStatus Status { get; protected set; }
-
         /// <summary>
         /// Initializes a new instance of the <see cref="GoalStructure{TBeliefSet}" /> class.
         /// </summary>
         /// <param name="children">The children of the goal structure.</param>
-        protected GoalStructure(IEnumerable<IGoalStructure<TBeliefSet>> children) => _children = children;
+        /// <param name="metadata">
+        /// Metadata about this goal, used to quickly display the goal in several contexts.
+        /// </param>
+        protected GoalStructure(IEnumerable<IGoalStructure<TBeliefSet>> children, Metadata? metadata = null)
+        {
+            _children = children;
+            Metadata = metadata ?? new Metadata();
+        }
 
         /// <summary>
         /// Gets the current goal using the given <see cref="IBeliefSet" />.
