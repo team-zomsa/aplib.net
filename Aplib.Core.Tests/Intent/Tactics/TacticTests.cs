9812b6d0584fd0c26f16c82e2de420502a6095c5
Luka <luukberkers@protonmail.com>
2024-06-03 12:05:48 +0200

    test: add failing guard test cases for `AnyOfTactic` and `FirstOfTactic`

--- Aplib.Core.Tests/Intent/Tactics/TacticTests.cs
+++ Aplib.Core.Tests/Intent/Tactics/TacticTests.cs
@@ -170,6 +170,21 @@ public class TacticTests
     }
 
     [Fact]
+    public void AnyOfTactic_WithFailingGuard_ReturnsNoAction()
+    {
+        // Arrange
+        Action<IBeliefSet> action1 = new(_ => { });
+        PrimitiveTactic<IBeliefSet> tactic1 = new(action1, _ => true);
+        AnyOfTactic<IBeliefSet> parentTactic = new(_ => false, tactic1);
+
+        // Act
+        IAction<IBeliefSet>? selectedAction = parentTactic.GetAction(It.IsAny<IBeliefSet>());
+
+        // Assert
+        selectedAction.Should().BeNull();
+    }
+
+    [Fact]
     public void FirstOfTacticTactic_WhenConstructed_HasExpectedData()
     {
         // Arrange
@@ -279,6 +294,21 @@ public class TacticTests
 
         // Assert
         selectedAction.Should().Be(action2);
+    }
+
+    [Fact]
+    public void FirstOfTactic_WithFailingGuard_ReturnsNoAction()
+    {
+        // Arrange
+        Action<IBeliefSet> action1 = new(_ => { });
+        PrimitiveTactic<IBeliefSet> tactic1 = new(action1, _ => true);
+        FirstOfTactic<IBeliefSet> parentTactic = new(_ => false, tactic1);
+
+        // Act
+        IAction<IBeliefSet>? selectedAction = parentTactic.GetAction(It.IsAny<IBeliefSet>());
+
+        // Assert
+        selectedAction.Should().BeNull();
     }
 
     /// <summary>
