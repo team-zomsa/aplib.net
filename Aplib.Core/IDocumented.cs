3632564d450067a6675f93f1dfc2e0ea6077427d
SilasPeters <69711739+SilasPeters@users.noreply.github.com>
2024-06-05 10:34:09 +0200

    docs: update IDocumented docs

--- Aplib.Core/IDocumented.cs
+++ Aplib.Core/IDocumented.cs
@@ -1,12 +1,12 @@
 namespace Aplib.Core
 {
     /// <summary>
-    /// Represents an object that contains an instance of <see cref="IMetadata"/>.
+    /// Represents an object that contains general information on an instance, such as <see cref="IMetadata"/>.
     /// </summary>
     public interface IDocumented
     {
         /// <summary>
-        /// Gets the metadata of this BDI cycle component.
+        /// Gets the metadata of the instance.
         /// </summary>
         /// <remark>
         /// This metadata may be useful for debugging or logging.
--- Aplib.Core/IMetadata.cs
+++ Aplib.Core/IMetadata.cs
@@ -3,22 +3,26 @@ using System;
 namespace Aplib.Core
 {
     /// <summary>
-    /// A collection of generic metadata for BDI cycle components.
+    /// A collection of generic metadata for unique instances which should help
+    /// visualise the instance with human-readable information.
     /// </summary>
+    /// <remark>
+    /// This metadata may be useful for debugging or logging.
+    /// </remark>
     public interface IMetadata
     {
         /// <summary>
-        /// Gets the unique identifier of the BDI cycle component.
+        /// Gets the unique identifier of the instance.
         /// </summary>
         public Guid Id { get; }
 
         /// <summary>
-        /// Gets the name used to display the BDI cycle component during debugging, logging, or general overviews.
+        /// Gets the name used to display the instance.
         /// </summary>
         public string? Name { get; }
 
         /// <summary>
-        /// Gets the description used to describe the BDI cycle component during debugging, logging, or general overviews.
+        /// Gets the description used to describe the instance.
         /// </summary>
         public string? Description { get; }
     }
