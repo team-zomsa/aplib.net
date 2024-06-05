8a8d1d6cf7b02db5cb5ef97c58483cd21be15e83
SilasPeters <69711739+SilasPeters@users.noreply.github.com>
2024-06-05 10:24:15 +0200

    chore: adhired to code standards

--- Aplib.Core/LiftingExtensionMethods.cs
+++ Aplib.Core/LiftingExtensionMethods.cs
@@ -15,79 +15,64 @@ namespace Aplib.Core
         /// <summary>
         /// Wraps a normal action into a tactic.
         /// </summary>
-        /// <returns>
-        /// A primitive tactic, whose guard always returns true.
-        /// If the action implements <see cref="IDocumented"/>, it's name will be prefixed with an indicator that it
-        /// has been lifted.
-        /// </returns>
+        /// <returns> A primitive tactic, whose guard always returns true. </returns>
         /// <param name="action">
         /// The action which on its own can function as a tactic. Meaning, the tactic consists of just a single action.
         /// </param>
         /// <param name="metadata">Optional metadata to be assigned to the tactic.</param>
-        public static PrimitiveTactic<TBeliefSet> Lift<TBeliefSet>(
-            this IAction<TBeliefSet> action,
-            IMetadata? metadata = null)
-            where TBeliefSet : IBeliefSet => metadata is null
-                ? new PrimitiveTactic<TBeliefSet>(action: action)
-                : new PrimitiveTactic<TBeliefSet>(metadata, action: action);
+        public static PrimitiveTactic<TBeliefSet> Lift<TBeliefSet>(this IAction<TBeliefSet> action, IMetadata metadata)
+            where TBeliefSet : IBeliefSet => new(metadata, action: action);
+
+        /// <inheritdoc cref="Lift{TBeliefSet}(IAction{TBeliefSet},IMetadata)" />
+        public static PrimitiveTactic<TBeliefSet> Lift<TBeliefSet>(this IAction<TBeliefSet> action)
+            where TBeliefSet : IBeliefSet => action.Lift(new Metadata());
 
         /// <summary>
         /// Wraps a queryable action into a tactic.
         /// </summary>
-        /// <returns>
-        /// A primitive tactic, whose guard always returns true.
-        /// If the action implements <see cref="IDocumented"/>, it's name will be prefixed with an indicator that it
-        /// has been lifted.
-        /// </returns>
+        /// <returns> A primitive tactic, whose guard always returns true. </returns>
         /// <param name="action">
         /// The action which on its own can function as a tactic. Meaning, the tactic consists of just a single action.
         /// </param>
         /// <param name="metadata">Optional metadata to be assigned to the tactic.</param>
-        public static PrimitiveTactic<TBeliefSet> Lift<TBeliefSet>(
-            this IQueryable<TBeliefSet> action,
-            IMetadata? metadata = null)
-            where TBeliefSet : IBeliefSet => metadata is null
-                ? new PrimitiveTactic<TBeliefSet>(queryAction: action)
-                : new PrimitiveTactic<TBeliefSet>(metadata, queryAction: action);
+        public static PrimitiveTactic<TBeliefSet> Lift<TBeliefSet>(this IQueryable<TBeliefSet> action, IMetadata metadata)
+            where TBeliefSet : IBeliefSet => new(metadata, queryAction: action);
+
+
+        /// <inheritdoc cref="Lift{TBeliefSet}(IQueryable{TBeliefSet},IMetadata)" />
+        public static PrimitiveTactic<TBeliefSet> Lift<TBeliefSet>(this IQueryable<TBeliefSet> action)
+            where TBeliefSet : IBeliefSet => action.Lift(new Metadata());
 
         /// <summary>
         /// Wraps a goal into a goal structure.
         /// </summary>
-        /// <returns>
-        /// A primitive goal structure.
-        /// If the goal implements <see cref="IDocumented"/>, it's name will be prefixed with an indicator that it
-        /// has been lifted.
-        /// </returns>
+        /// <returns> A primitive goal structure. </returns>
         /// <param name="goal">
         /// The goal which on its own can function as a goal structure. Meaning, the goal structure consists of just a
         /// single goal.
         /// </param>
         /// <param name="metadata">Optional metadata to be assigned to the goal structure.</param>
-        public static PrimitiveGoalStructure<TBeliefSet> Lift<TBeliefSet>(
-            this IGoal<TBeliefSet> goal,
-            IMetadata? metadata = null)
-            where TBeliefSet : IBeliefSet => metadata is null
-            ? new PrimitiveGoalStructure<TBeliefSet>(goal)
-            : new PrimitiveGoalStructure<TBeliefSet>(metadata, goal);
+        public static PrimitiveGoalStructure<TBeliefSet> Lift<TBeliefSet>(this IGoal<TBeliefSet> goal, IMetadata metadata)
+            where TBeliefSet : IBeliefSet => new(metadata, goal);
+
+        /// <inheritdoc cref="Lift{TBeliefSet}(Desire.Goals.IGoal{TBeliefSet},IMetadata)" />
+        public static PrimitiveGoalStructure<TBeliefSet> Lift<TBeliefSet>(this IGoal<TBeliefSet> goal)
+            where TBeliefSet : IBeliefSet => goal.Lift(new Metadata());
 
         /// <summary>
         /// Wraps a goal structure into a desire set.
         /// </summary>
-        /// <returns>
-        /// A desire set.
-        /// If the goal structure implements <see cref="IDocumented"/>, it's name will be prefixed with an indicator
-        /// that it has been lifted.
-        /// </returns>
+        /// <returns> A desire set. </returns>
         /// <param name="goalStructure">
         /// The goal structure which on its own can function as a desire set. Meaning, the desire set consists of just
         /// a single goal structure.
         /// </param>
         /// <param name="metadata">Optional metadata to be assigned to the desire set.</param>
-        public static DesireSet<TBeliefSet> Lift<TBeliefSet>(
-            this IGoalStructure<TBeliefSet> goalStructure,
-            IMetadata? metadata = null)
-            where TBeliefSet : IBeliefSet => metadata is null
-            ? new DesireSet<TBeliefSet>(goalStructure)
-            : new DesireSet<TBeliefSet>(metadata, goalStructure);
+        public static DesireSet<TBeliefSet> Lift<TBeliefSet>(this IGoalStructure<TBeliefSet> goalStructure, IMetadata metadata)
+            where TBeliefSet : IBeliefSet => new(metadata, goalStructure);
+
+        /// <inheritdoc cref="Lift{TBeliefSet}(Desire.GoalStructures.IGoalStructure{TBeliefSet},IMetadata)" />
+        public static DesireSet<TBeliefSet> Lift<TBeliefSet>(this IGoalStructure<TBeliefSet> goalStructure)
+            where TBeliefSet : IBeliefSet => goalStructure.Lift(new Metadata());
     }
 }
