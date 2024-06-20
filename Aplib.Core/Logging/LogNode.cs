11b55397feafbc8dae1ecfd09cdc2617dabdbf02
Joen <90771356+JoenvandeVorle@users.noreply.github.com>
2024-06-20 19:26:53 +0200

    chore: update documentation
    Co-authored-by: Jens Steenmetz <35835627+JensSteenmetz@users.noreply.github.com>

--- Aplib.Core/Logging/LogNode.cs
+++ Aplib.Core/Logging/LogNode.cs
@@ -28,8 +28,11 @@ namespace Aplib.Core.Logging
         /// </summary>
         /// <param name="loggable">The loggable object of the node.</param>
         /// <param name="depth">The depth at which this node resides.</param>
-        /// <param name="children">The children of the node. If omitted, an empty list will be used.</param>
-        /// <remarks> It is assumed the children have a correct depth set. </remarks>
+        /// <param name="children">
+        /// The children of the node.
+        /// It is assumed the children have a correct depth set.
+        /// If omitted, an empty list will be used.
+        /// </param>
         public LogNode(ILoggable loggable, int depth, List<LogNode> children)
         {
             Loggable = loggable;
