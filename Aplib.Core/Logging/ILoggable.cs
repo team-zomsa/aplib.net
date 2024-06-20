97fe7700b2ac00dd83a6a9e5eb76f7be5460eb86
Joen <90771356+JoenvandeVorle@users.noreply.github.com>
2024-06-20 16:54:52 +0200

    chore: apply suggestions from code review
    Co-authored-by: Luka <51298607+LuukBerkers@users.noreply.github.com>

--- Aplib.Core/Logging/ILoggable.cs
+++ Aplib.Core/Logging/ILoggable.cs
@@ -4,8 +4,8 @@ namespace Aplib.Core.Logging
 {
     /// <summary>
     /// An interface that allows defining the structure of loggable objects through a <see cref="LogNode"/> tree.
-    /// The only method that needs to be implemented in loggable classes is the GetLogChildren method;
-    /// the structure of the log tree is generated automatically when calling the GetLogTree method.
+    /// The only method that needs to be implemented in loggable classes is the <see cref="GetLogChildren" /> method;
+    /// the structure of the log tree is generated automatically when calling the <see cref="GetLogTree" /> method.
     /// </summary>
     public interface ILoggable : IDocumented
     {
@@ -18,8 +18,8 @@ namespace Aplib.Core.Logging
         /// <summary>
         /// Generates a log tree of the loggable object.
         /// </summary>
-        /// <param name="depth">The depth of the this node in log tree.</param>
-        /// <returns>The first node of the log tree.</returns>
+        /// <param name="depth">The depth of this node in the log tree.</param>
+        /// <returns>The root node of the log tree.</returns>
         public LogNode GetLogTree(int depth = 0)
         {
             LogNode root = new(this, depth);
