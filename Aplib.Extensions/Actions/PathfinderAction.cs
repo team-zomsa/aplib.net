aab39fe1cdecd0417eda8caf89e594fbb1250c71
Joachim Dekker <jdekker3008@gmail.com>
2024-06-22 17:33:17 +0200

    Reformat file

--- Aplib.Extensions/Actions/PathfinderAction.cs
+++ Aplib.Extensions/Actions/PathfinderAction.cs
@@ -1,23 +1,33 @@
 using Aplib.Core;
 using Aplib.Core.Belief.BeliefSets;
-using Aplib.Core.Intent.Actions;
+using System;
 
 namespace Aplib.Extensions.Actions
 {
-    public class PathfinderAction<TBeliefset, T> : Action<TBeliefset>
+    public class PathfinderAction<TBeliefset, T> : Core.Intent.Actions.Action<TBeliefset>
         where TBeliefset : IBeliefSet
     {
-        public PathfinderAction(IMetadata metadata, IPathfinder<T> pathfinder, System.Func<TBeliefset, T> getCurrentLocation, System.Func<TBeliefset, T> getEndLocation, System.Action<TBeliefset, T> effect)
+        public PathfinderAction(IMetadata metadata,
+            IPathfinder<T> pathfinder,
+            Func<TBeliefset, T> getCurrentLocation,
+            Func<TBeliefset, T> getEndLocation,
+            Action<TBeliefset, T> effect)
             : base(metadata, ExecutePathFinder(pathfinder, getCurrentLocation, getEndLocation, effect))
         {
         }
 
-        public PathfinderAction(IPathfinder<T> pathfinder, System.Func<TBeliefset, T> getCurrentLocation, System.Func<TBeliefset, T> getEndLocation, System.Action<TBeliefset, T> effect)
+        public PathfinderAction(IPathfinder<T> pathfinder,
+            Func<TBeliefset, T> getCurrentLocation,
+            Func<TBeliefset, T> getEndLocation,
+            Action<TBeliefset, T> effect)
             : this(null, pathfinder, getCurrentLocation, getEndLocation, effect)
         {
         }
 
-        private static System.Action<TBeliefset> ExecutePathFinder(IPathfinder<T> pathfinder,System.Func<TBeliefset, T> getCurrentLocation, System.Func<TBeliefset, T> getEndLocation, System.Action<TBeliefset, T> effect) => (set =>
+        private static Action<TBeliefset> ExecutePathFinder(IPathfinder<T> pathfinder,
+            Func<TBeliefset, T> getCurrentLocation,
+            Func<TBeliefset, T> getEndLocation,
+            Action<TBeliefset, T> effect) => set =>
         {
             T startLocation = getCurrentLocation(set);
             T endLocation = getEndLocation(set);
@@ -28,6 +38,6 @@ namespace Aplib.Extensions.Actions
             }
 
             effect(set, nextLocation);
-        });
+        };
     }
 }
