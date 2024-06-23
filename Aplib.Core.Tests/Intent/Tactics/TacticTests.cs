cfff98032b032a9ddb851e1e14a2a85288eb7d9f
Jens Steenmetz <35835627+JensSteenmetz@users.noreply.github.com>
2024-06-23 19:05:53 +0200

    refactor: make `FirstOfTactic` derive `Tactic` (#72)
    * refactor: make FirstOfTactic derive Tactic
    
    * feat: make subtactics protected internal
    
    * refactor: rename forgotten subTactics to subtactics

--- Aplib.Core.Tests/Intent/Tactics/TacticTests.cs
+++ Aplib.Core.Tests/Intent/Tactics/TacticTests.cs
@@ -34,25 +34,25 @@ public class TacticTests
     {
         public System.Predicate<IBeliefSet> Guard => _guard;
 
-        public LinkedList<ITactic<IBeliefSet>> SubTactics => _subTactics;
+        public LinkedList<ITactic<IBeliefSet>> Subtactics => _subtactics;
 
         public TestFirstOfTactic
-            (Metadata metadata, System.Predicate<IBeliefSet> guard, params ITactic<IBeliefSet>[] subTactics)
-            : base(metadata, guard, subTactics)
+            (Metadata metadata, System.Predicate<IBeliefSet> guard, params ITactic<IBeliefSet>[] subtactics)
+            : base(metadata, guard, subtactics)
         {
         }
 
-        public TestFirstOfTactic(Metadata metadata, params ITactic<IBeliefSet>[] subTactics)
-            : base(metadata, subTactics)
+        public TestFirstOfTactic(Metadata metadata, params ITactic<IBeliefSet>[] subtactics)
+            : base(metadata, subtactics)
         {
         }
 
-        public TestFirstOfTactic(System.Predicate<IBeliefSet> guard, params ITactic<IBeliefSet>[] subTactics)
-            : base(guard, subTactics)
+        public TestFirstOfTactic(System.Predicate<IBeliefSet> guard, params ITactic<IBeliefSet>[] subtactics)
+            : base(guard, subtactics)
         {
         }
 
-        public TestFirstOfTactic(params ITactic<IBeliefSet>[] subTactics) : base(subTactics) { }
+        public TestFirstOfTactic(params ITactic<IBeliefSet>[] subtactics) : base(subtactics) { }
     }
 
 
@@ -193,15 +193,15 @@ public class TacticTests
         // Arrange
         Metadata metadata = It.IsAny<Metadata>();
         System.Predicate<IBeliefSet> guard = It.IsAny<System.Predicate<IBeliefSet>>();
-        ITactic<IBeliefSet>[] subTactics = [It.IsAny<ITactic<IBeliefSet>>()];
+        ITactic<IBeliefSet>[] subtactics = [It.IsAny<ITactic<IBeliefSet>>()];
 
         // Act
-        TestFirstOfTactic tactic = new(metadata, guard, subTactics);
+        TestFirstOfTactic tactic = new(metadata, guard, subtactics);
 
         // Assert
         tactic.Metadata.Should().Be(metadata);
         tactic.Guard.Should().Be(guard);
-        tactic.SubTactics.Should().BeEquivalentTo(subTactics);
+        tactic.Subtactics.Should().BeEquivalentTo(subtactics);
     }
 
     [Fact]
@@ -209,15 +209,15 @@ public class TacticTests
     {
         // Arrange
         Metadata metadata = It.IsAny<Metadata>();
-        ITactic<IBeliefSet>[] subTactics = [It.IsAny<ITactic<IBeliefSet>>()];
+        ITactic<IBeliefSet>[] subtactics = [It.IsAny<ITactic<IBeliefSet>>()];
 
         // Act
-        TestFirstOfTactic tactic = new(metadata, subTactics);
+        TestFirstOfTactic tactic = new(metadata, subtactics);
 
         // Assert
         tactic.Metadata.Should().Be(metadata);
         tactic.Guard(It.IsAny<IBeliefSet>()).Should().BeTrue();
-        tactic.SubTactics.Should().BeEquivalentTo(subTactics);
+        tactic.Subtactics.Should().BeEquivalentTo(subtactics);
     }
 
     [Fact]
@@ -225,34 +225,34 @@ public class TacticTests
     {
         // Arrange
         System.Predicate<IBeliefSet> guard = It.IsAny<System.Predicate<IBeliefSet>>();
-        ITactic<IBeliefSet>[] subTactics = [It.IsAny<ITactic<IBeliefSet>>()];
+        ITactic<IBeliefSet>[] subtactics = [It.IsAny<ITactic<IBeliefSet>>()];
 
         // Act
-        TestFirstOfTactic tactic = new(guard, subTactics);
+        TestFirstOfTactic tactic = new(guard, subtactics);
 
         // Assert
         tactic.Metadata.Id.Should().NotBeEmpty();
         tactic.Metadata.Name.Should().BeNull();
         tactic.Metadata.Description.Should().BeNull();
         tactic.Guard.Should().Be(guard);
-        tactic.SubTactics.Should().BeEquivalentTo(subTactics);
+        tactic.Subtactics.Should().BeEquivalentTo(subtactics);
     }
 
     [Fact]
     public void FirstOfTacticTactic_WithoutMetadataWithoutGuard_HasExpectedData()
     {
         // Arrange
-        ITactic<IBeliefSet>[] subTactics = [It.IsAny<ITactic<IBeliefSet>>()];
+        ITactic<IBeliefSet>[] subtactics = [It.IsAny<ITactic<IBeliefSet>>()];
 
         // Act
-        TestFirstOfTactic tactic = new(subTactics);
+        TestFirstOfTactic tactic = new(subtactics);
 
         // Assert
         tactic.Metadata.Id.Should().NotBeEmpty();
         tactic.Metadata.Name.Should().BeNull();
         tactic.Metadata.Description.Should().BeNull();
         tactic.Guard(It.IsAny<IBeliefSet>()).Should().BeTrue();
-        tactic.SubTactics.Should().BeEquivalentTo(subTactics);
+        tactic.Subtactics.Should().BeEquivalentTo(subtactics);
     }
 
     /// <summary>
