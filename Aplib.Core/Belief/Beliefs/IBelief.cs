74aba910f13823717ac05a55ae768d0de17d236d
Luka <luukberkers@protonmail.com>
2024-06-24 13:44:14 +0200

    fix(IBelief): improve comment clarity for interface and its method

--- Aplib.Core/Belief/Beliefs/IBelief.cs
+++ Aplib.Core/Belief/Beliefs/IBelief.cs
@@ -1,12 +1,13 @@
 ﻿namespace Aplib.Core.Belief.Beliefs
 {
     /// <summary>
-    /// A belief represents/encapsulates an observation (i.e., piece of information of the game state as perceived by an agent).
+    /// A belief represents/encapsulates an observation,
+    /// i.e., information about the game state as perceived by an agent.
     /// </summary>
     public interface IBelief
     {
         /// <summary>
-        /// Updates the belief based on information of the game state.
+        /// Updates the belief based on information about the game state.
         /// </summary>
         void UpdateBelief();
     }
