344dea43e346b750dc901dd5a955b259a3dcbf03
Luka <luukberkers@protonmail.com>
2024-06-24 13:15:23 +0200

    fix(IAgent): update comments to clarify agent testing behavior

--- Aplib.Core/Agents/IAgent.cs
+++ Aplib.Core/Agents/IAgent.cs
@@ -1,14 +1,14 @@
 namespace Aplib.Core.Agents
 {
     /// <summary>
-    /// Defines an agent that can play a game.
+    /// Defines a testing agent that can be updated for every frame of the game.
     /// </summary>
     public interface IAgent : ICompletable
     {
         /// <summary>
-        /// Updates the agent's state and goals.
+        /// Updates the agent's beliefs, desires, and intentions.
         /// </summary>
-        /// <remarks>This method will get called every frame of the game.</remarks>
+        /// <remarks>This method will get called for every frame of the game.</remarks>
         public void Update();
     }
 }
